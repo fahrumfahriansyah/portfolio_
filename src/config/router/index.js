a79a6f1522751e558ccef5b026d598f78a363ed4
fahrumfahriansyah <fahrumfahriansyah1@gmail.com>
2023-01-29 09:11:22 +0700

    menambahkan favicon

--- src/config/router/index.js
+++ src/config/router/index.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Open, Home, Register, Login, About, Admin, Contacts } from '../../pages'
-
 const Index = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route>
-                    <Route path='/' element={<Open />} />
-                    <Route path='/Portfolio/' element={<Open />} />
-                    <Route path='/home' element={<Home />} />
-                    <Route path='*' element={<Admin />} />
-                    <Route path='/register' element={<Register />} />
-                    <Route path='/Login' element={<Login />} />
-                    <Route path='/About' element={<About />} />
-                    <Route path='/Contacts' element={<Contacts />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <>
+
+            <BrowserRouter>
+                <Routes>
+                    <Route>
+                        <Route path='/' element={<Open />} />
+                        <Route path='/Portfolio/' element={<Open />} />
+                        <Route path='/home' element={<Home />} />
+                        <Route path='*' element={<Admin />} />
+                        <Route path='/register' element={<Register />} />
+                        <Route path='/Login' element={<Login />} />
+                        <Route path='/About' element={<About />} />
+                        <Route path='/Contacts' element={<Contacts />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
     )
 }
 
